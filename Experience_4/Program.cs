2023143550733c26fa1374dd637bf2ddcbf528db
Vutaly <Vutaly@DESKTOP-RSVQN6H>
2023-01-13 19:24:49 +0300

    Добавьте файлы проекта.

---
+++ Experience_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experience_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада: ");
+            double sum = Convert.ToDouble(Console.ReadLine());
+
+            if (sum < 100)
+            {
+                sum += sum * 0.05;
+            }
+            else if (sum <= 200)
+            {
+                sum += sum * 0.07;
+            }
+            else
+            {
+                sum += sum * 0.1;
+            }
+            Console.WriteLine($"Сумму вклада после начисления процентов: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
